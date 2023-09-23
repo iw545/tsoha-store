7c0d44924e8cdc7a31651b3cede6cfb05da8194d
iw545 <leo.ihalainen@windowslive.com>
2023-09-23 21:37:55 +0300

    Create first version

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+
+app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def index():
+    db.session.execute(text("INSERT INTO visitors (time) VALUES (NOW())"))
+    db.session.commit()
+    result = db.session.execute(text("SELECT COUNT(*) FROM visitors"))
+    counter = result.fetchone()[0]
+    return render_template("index.html", counter=counter)
