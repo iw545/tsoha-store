339152791233ab67aa799f2015e08a336d6060fc
iw545 <leo.ihalainen@windowslive.com>
2023-09-24 22:39:00 +0300

    Schema.sql updated

--- schema.sql
+++ schema.sql
@@ -1,1 +1,3 @@
-CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
+CREATE TABLE items (id SERIAL PRIMARY KEY, name TEXT, price NUMERIC(7,2), category TEXT, 
+time DATE, sold INT, grades NUMERIC(3,2));
+CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT, password TEXT, admin BOOLEAN DEFAULT FALSE); 
