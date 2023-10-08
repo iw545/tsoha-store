f607d360cf936ac018b1bf0b1d7d0b9be99ae36c
iw545 <leo.ihalainen@windowslive.com>
2023-10-08 18:40:49 +0300

    Partial cart, item purchase and item review system added

--- routes.py
+++ routes.py
@@ -12,6 +12,7 @@ def index():
     counter = visits.get_counter()
     return render_template("index.html", counter=counter)
 
+
 @app.route("/login", methods=["POST"])
 def login():
 
@@ -27,6 +28,7 @@ def login():
 
         if check_password_hash(hash_value, password):
             session["username"] = username
+            session["user_id"] = user.id
             return redirect("/")
     
         else:
@@ -36,6 +38,7 @@ def login():
     else:
         # username not found
         return redirect("/")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -70,6 +73,7 @@ def logout():
     # todo: empty cart automatically if it has items
     return redirect("/")
 
+
 @app.route("/search", methods=["GET", "POST"])
 def search():
     if request.method == "POST":
@@ -77,39 +81,121 @@ def search():
         if len(search_term) == 0:
             return redirect("/catalog")
         
-        sql = text("SELECT name, price FROM items WHERE name ILIKE :search_term")
+        sql = text("SELECT id, name, price FROM items WHERE name ILIKE :search_term")
         result = db.session.execute(sql, {"search_term": "%" + search_term + "%"})
-
-        items_list = []
-        for item in result.fetchall():
-            items_list.append((item[0], item[1]))
+        items_list = result.fetchall()
 
         return render_template("search.html", items=items_list)
 
     return redirect("/catalog")
 
+
 @app.route("/catalog")
 def catalog():
     # todo: add item sorting by price, category, reviews, date added, most sold
-    sql = text("SELECT name, price FROM items")
+    sql = text("SELECT id, name, price FROM items")
     result = db.session.execute(sql)
-
-    items_list = []
-    for item in result.fetchall():
-        items_list.append((item[0], item[1]))
+    items_list = result.fetchall()
 
     return render_template("catalog.html", items=items_list)
 
+
+@app.route("/item/<int:item_id>")
+def item_page(item_id):
+    current_user = session.get("user_id")
+    user_can_review = False
+
+    sql_item = text("SELECT * FROM items WHERE id = :item_id")
+    item = db.session.execute(sql_item, {"item_id": item_id}).fetchone()
+
+    # todo: fix, doesnt show reviews on item page
+    sql_reviews = text("SELECT user_id, item_id, content, grade FROM reviews WHERE item_id = :item_id")
+    reviews = db.session.execute(sql_reviews, {"item_id": item_id})
+    reviews_list = reviews.fetchall()
+
+    if current_user:
+        # todo: check if user has bought item before
+        # to allow them to write a review for an item
+
+        #sql_user = text("SELECT user_id FROM bought WHERE user_id = :user_id AND item_id = :item_id")
+        # is_bought = db.session.execute(sql_user, {"user_id": current_user, "item_id": item_id})
+
+        # if is_bought.fetchone():
+        user_can_review = True
+
+    return render_template("item_page.html", item=item, reviews_list=reviews_list, user_can_review=user_can_review)
+
+
 @app.route("/cart")
 def cart():
-    # todo: add options:
-    # - add / remove item to / from cart
-    # - view cart contents
-    # - purchase items in cart, add order to users purchase history
-    return render_template("cart.html")
+    current_user = session.get("user_id")
+    if current_user:
+        sql_cart = text("SELECT item_id FROM cart")
+        cart_items = db.session.execute(sql_cart)
+
+        return render_template("cart.html", cart_items=cart_items)
+
+    # not logged in as a user
+    # display message to log in
+    return redirect("/")
+
+
+@app.route("/update_cart", methods=["POST"])
+def update_cart():
+    option = request.form.get("option")
+    item_id = request.form.get("item_id")
+
+    # add item to cart
+    if option == "add":
+        sql_item = text("INSERT INTO cart (item_id) VALUES (:item_id)")
+        db.session.execute(sql_item, {"item_id": item_id})
+        db.session.commit()
+
+    # remove item from cart
+    # todo: display item quantities and allow multiple of same item to be added
+    elif option == "remove":
+        sql_item = text("DELETE FROM cart WHERE item_id = :item_id")
+        db.session.execute(sql_item, {"item_id": item_id})
+        db.session.commit()
+        
+    return redirect("/cart")
+
+
+@app.route("/purchase_items", methods=["POST"])
+def purchase():
+
+    sql_cart = text("SELECT user_id, item_id FROM cart")
+    cart_items = db.session.execute(sql_cart)
+    # add the cart items to bought table
+    for item in cart_items:
+        sql_item = text("INSERT INTO bought (user_id, item_id, time) VALUES (:user_id, :item_id, current_date)")
+        db.session.execute(sql_item, {"item_id": item.item_id})
+    db.session.commit()
+
+    # after the above, remove items from cart
+    sql_cart = text("DELETE FROM cart")
+    db.session.execute(sql_cart)
+    db.session.commit()
+
+    return render_template("purchase_made.html")
+
+
+@app.route("/post_review", methods=["POST"])
+def user_review():
+    content = request.form.get("content")
+    grade = request.form.get("grade")
+
+    sql_review = text("INSERT INTO reviews (content, grade) VALUES (:content, :grade)")
+    db.session.execute(sql_review, {"content": content, "grade": grade})
+    db.session.commit()
+
+    return redirect("/")
+
 
 @app.route("/usersettings")
 def usersettings():
     # todo: add option to view previous orders made by user,
     # show item reviews written by user
+    # if admin user: add functionality to add and remove
+    # items from the database via admin settings section
     return render_template("usersettings.html")
--- schema.sql
+++ schema.sql
@@ -1,3 +1,6 @@
 CREATE TABLE items (id SERIAL PRIMARY KEY, name TEXT, price NUMERIC(7,2), category TEXT, 
 time DATE, sold INT, grades NUMERIC(3,2));
 CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT, password TEXT, admin BOOLEAN DEFAULT FALSE); 
+CREATE TABLE bought (id SERIAL PRIMARY KEY, user_id INT REFERENCES users(id), item_id INT REFERENCES items(id), quantity INT, time DATE);
+CREATE TABLE cart (id SERIAL PRIMARY KEY, item_id INT REFERENCES items(id), quantity INT, user_id INT REFERENCES users(id));
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, item_id INT REFERENCES items(id), user_id INT REFERENCES users(id), content TEXT, grade INT);
