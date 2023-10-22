5a0f19bd250b84145479ad2c4d1e2ec0ae0c861c
iw545 <leo.ihalainen@windowslive.com>
2023-10-22 23:21:08 +0300

    Fixed review and cart system, added admin settings

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from app import app
 from db import db
 import visits
 from sqlalchemy.sql import text
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
@@ -77,7 +77,6 @@ def register():
 def logout():
     if session:
         del session["username"]
-    # todo: empty cart automatically if it has items
     return redirect("/")
 
 
@@ -104,22 +103,28 @@ def catalog():
     sort_type = request.args.get("sort_type")
 
     if sort_type == "name_asc":
-        sql = text("SELECT id, name, price, category, grade FROM items ORDER BY name ASC")
+        sql = text("""SELECT id, name, price, category, 
+                   grade FROM items ORDER BY name ASC""")
 
     elif sort_type == "name_desc":
-        sql = text("SELECT id, name, price, category, grade FROM items ORDER BY name DESC")
+        sql = text("""SELECT id, name, price, category, 
+                   grade FROM items ORDER BY name DESC""")
 
     elif sort_type == "price_asc":
-        sql = text("SELECT id, name, price, category, grade FROM items ORDER BY price ASC")
+        sql = text("""SELECT id, name, price, category, 
+                   grade FROM items ORDER BY price ASC""")
 
     elif sort_type == "price_desc":
-        sql = text("SELECT id, name, price, category, grade FROM items ORDER BY price DESC")
+        sql = text("""SELECT id, name, price, category, 
+                   grade FROM items ORDER BY price DESC""")
 
     elif sort_type == "most_sold":
-        sql = text("SELECT id, name, price, category, grade FROM items ORDER BY sold DESC")
+        sql = text("""SELECT id, name, price, category, 
+                   grade FROM items ORDER BY sold DESC""")
 
     elif sort_type == "grade":
-        sql = text("SELECT id, name, price, category, grade FROM items ORDER BY grade DESC")
+        sql = text("""SELECT id, name, price, category, 
+                   grade FROM items ORDER BY grade DESC""")
 
     else:
         sql = text("SELECT id, name, price, category, grade FROM items")
@@ -132,115 +137,217 @@ def catalog():
 
 @app.route("/item/<int:item_id>")
 def item_page(item_id):
-    current_user = session.get("user_id")
+    user_id = session.get("user_id")
     user_can_review = False
 
     sql_item = text("SELECT * FROM items WHERE id = :item_id")
     item = db.session.execute(sql_item, {"item_id": item_id}).fetchone()
 
-    # todo: fix, doesnt show reviews on item page
-    sql_reviews = text("SELECT user_id, item_id, content, grade FROM reviews WHERE item_id = :item_id")
-    reviews = db.session.execute(sql_reviews, {"item_id": item_id})
-    reviews_list = reviews.fetchall()
+    sql_grade = text("SELECT ROUND(AVG(grade),1) FROM reviews WHERE item_id = :item_id")
+    avg_grade = db.session.execute(sql_grade, {"item_id": item_id}).scalar()
+    if avg_grade == None:
+        avg_grade = "-"
 
-    if current_user:
-        # todo: check if user has bought item before
-        # to allow them to write a review for an item
+    sql_grades = text("SELECT COUNT(grade) FROM reviews WHERE item_id = :item_id")
+    grades = db.session.execute(sql_grades, {"item_id": item_id}).scalar()
 
-        #sql_user = text("SELECT user_id FROM bought WHERE user_id = :user_id AND item_id = :item_id")
-        # is_bought = db.session.execute(sql_user, {"user_id": current_user, "item_id": item_id})
+    sql_reviews = text("""SELECT users.username, reviews.item_id, reviews.content, reviews.grade 
+    FROM reviews JOIN users ON users.id = reviews.user_id WHERE reviews.item_id = :item_id""")
+    reviews = db.session.execute(sql_reviews, {"item_id": item_id}).fetchall()
 
-        # if is_bought.fetchone():
-        user_can_review = True
+    if user_id:
 
-    return render_template("item_page.html", item=item, reviews_list=reviews_list, user_can_review=user_can_review)
+        sql_user = text("SELECT user_id FROM bought WHERE user_id = :user_id AND item_id = :item_id")
+        is_bought = db.session.execute(sql_user, {"user_id": user_id, "item_id": item_id})
+
+        if is_bought.fetchone():
+            user_can_review = True
+
+    return render_template("item_page.html", item=item, reviews=reviews, user_can_review=user_can_review, avg_grade=avg_grade, grades=grades)
 
 
 @app.route("/cart")
 def cart():
     notification = ""
-    current_user = session.get("user_id")
-    if current_user:
-        sql_cart = text("SELECT item_id FROM cart")
-        cart_items = db.session.execute(sql_cart)
+    user_id = session.get("user_id")
+    if user_id:
+        sql_cart = text("""SELECT items.id, items.name, items.price 
+        FROM cart JOIN items ON cart.item_id = items.id WHERE cart.user_id = :user_id""")
+        cart_items = db.session.execute(sql_cart, {"user_id": user_id}).fetchall()
 
         return render_template("cart.html", cart_items=cart_items)
     
     notification = "Kirjaudu sisään nähdäksesi ostoskorin."
-    return render_template("/index.html", notification=notification)
+    return render_template("index.html", notification=notification)
 
 
 @app.route("/update_cart", methods=["POST"])
 def update_cart():
-    notification = ""
     option = request.form.get("option")
     item_id = request.form.get("item_id")
+    user_id = session.get("user_id")
 
-    # add item to cart
     if option == "add":
-        sql_item = text("INSERT INTO cart (item_id) VALUES (:item_id)")
-        db.session.execute(sql_item, {"item_id": item_id})
+        sql_item = text("INSERT INTO cart (item_id, user_id) VALUES (:item_id, :user_id)")
+        db.session.execute(sql_item, {"item_id": item_id, "user_id": user_id})
         db.session.commit()
 
-    # remove item from cart
-    # todo: display item quantities and allow multiple of same item to be added
     elif option == "remove":
-        sql_item = text("DELETE FROM cart WHERE item_id = :item_id")
-        db.session.execute(sql_item, {"item_id": item_id})
+        sql_item = text("DELETE FROM cart WHERE item_id = :item_id AND user_id = :user_id")
+        db.session.execute(sql_item, {"item_id": item_id, "user_id": user_id})
         db.session.commit()
 
-    notification = "Ostoskori päivitetty."
-    return render_template("cart.html", notification=notification)
+    return redirect(url_for("item_page", item_id=item_id))
 
 
 @app.route("/purchase_items", methods=["POST"])
 def purchase():
-
     notification = ""
-    sql_cart = text("SELECT user_id, item_id FROM cart")
-    cart_items = db.session.execute(sql_cart)
+    user_id = session.get("user_id")
+    sql_cart = text("SELECT user_id, item_id FROM cart WHERE user_id = :user_id")
+    cart_items = db.session.execute(sql_cart, {"user_id": user_id}).fetchall()
+
     if len(cart_items) == 0:
         notification = "Ostoskori tyhjä, ostosta ei voi suorittaa."
         return render_template("cart.html", notification=notification)
 
-    # add the cart items to bought table
     for item in cart_items:
-        sql_item = text("INSERT INTO bought (user_id, item_id, time) VALUES (:user_id, :item_id, current_date)")
-        db.session.execute(sql_item, {"item_id": item.item_id})
-    db.session.commit()
+        sql_item = text("""INSERT INTO bought (user_id, item_id, time) 
+                        VALUES (:user_id, :item_id, current_date)""")
+        db.session.execute(sql_item, {"item_id": item.item_id, "user_id": user_id})
 
-    # after the above, remove items from cart
-    sql_cart = text("DELETE FROM cart")
-    db.session.execute(sql_cart)
+        sql_item_sold = text("""UPDATE items SET sold = sold+1 WHERE id = :item_id""")
+        db.session.execute(sql_item_sold, {"item_id": item.item_id})
+
+        db.session.commit()
+
+    sql_cart = text("DELETE FROM cart WHERE user_id = :user_id")
+    db.session.execute(sql_cart, {"user_id": user_id})
     db.session.commit()
 
     return render_template("purchase_made.html")
 
 
-@app.route("/post_review", methods=["GET"])
+@app.route("/post_review", methods=["POST"])
 def user_review():
+    user_id = session.get("user_id")
+    item_id = request.form.get("item_id")
     content = request.form.get("content")
     grade = request.form.get("grade")
 
-    sql_review = text("INSERT INTO reviews (content, grade) VALUES (:content, :grade)")
-    db.session.execute(sql_review, {"content": content, "grade": grade})
+    sql_review = text("""INSERT INTO reviews (user_id, item_id, content, grade) 
+                      VALUES (:user_id, :item_id, :content, :grade)""")
+    db.session.execute(sql_review, {"user_id": user_id, "item_id": item_id, "content": content, "grade": grade})
     db.session.commit()
 
-    return redirect("/")
+    return redirect(url_for("item_page", item_id=item_id))
 
 
 @app.route("/usersettings")
 def usersettings():
-    # todo: add option to view previous orders made by user,
-    # show item reviews written by user
-    # if admin user: add functionality to add and remove
-    # items from the database via admin settings section
 
     notification = ""
-    current_user = session.get("user_id")
-    if current_user:
+    is_admin = False
+    user_id = session.get("user_id")
+    admin_check = text("SELECT admin FROM users WHERE id = :user_id")
+    admin_user = db.session.execute(admin_check, {"user_id": user_id}).scalar()
 
-        return render_template("usersettings.html")
+    if admin_user == True:
+        is_admin = True
+
+    if user_id:
+
+        return render_template("usersettings.html", is_admin=is_admin)
     
     notification = "Kirjaudu sisään nähdäksesi käyttäjäasetukset."
     return render_template("index.html", notification=notification)
+
+
+@app.route("/user_reviews")
+def user_reviews():
+
+    notification = ""
+    user_id = session.get("user_id")
+    if user_id:
+        sql = text("""SELECT reviews.*, items.name AS item_name
+                    FROM reviews JOIN items ON reviews.item_id = items.id 
+                   WHERE reviews.user_id = :user_id""")
+        user_reviews = db.session.execute(sql, {"user_id": user_id}).fetchall()
+
+        return render_template("user_reviews.html", user_reviews=user_reviews)
+    
+    notification = "Kirjaudu sisään nähdäksesi käyttäjäasetukset."
+    return render_template("index.html", notification=notification)
+
+
+@app.route("/user_purchases")
+def user_purchases():
+
+    notification = ""
+    user_id = session.get("user_id")
+    if user_id:
+        sql = text("""SELECT items.*, bought.time AS purchase_time
+                    FROM items JOIN bought ON items.id = bought.item_id
+                    WHERE bought.user_id = :user_id""")
+        user_purchases = db.session.execute(sql, {"user_id": user_id}).fetchall()
+        
+        return render_template("user_purchases.html", user_purchases=user_purchases)
+    
+    notification = "Kirjaudu sisään nähdäksesi käyttäjäasetukset."
+    return render_template("index.html", notification=notification)
+
+
+@app.route("/all_purchases")
+def all_purchases():
+
+    is_admin = False
+    user_id = session.get("user_id")
+    admin_check = text("SELECT admin FROM users WHERE id = :user_id")
+    admin_user = db.session.execute(admin_check, {"user_id": user_id}).scalar()
+
+    if admin_user == True:
+        is_admin = True
+        sql = text("""SELECT items.*, bought.time AS purchase_time
+                    FROM items JOIN bought ON items.id = bought.item_id""")
+        all_purchases = db.session.execute(sql).fetchall()
+        
+        return render_template("all_purchases.html", all_purchases=all_purchases, is_admin=is_admin)
+    
+    notification = "Kirjaudu sisään nähdäksesi käyttäjäasetukset."
+    return render_template("index.html", notification=notification)
+
+
+@app.route("/item_tools", methods=["GET", "POST"])
+def item_tools():
+    notification = ""
+    is_admin = False
+    user_id = session.get("user_id")
+    admin_check = text("SELECT admin FROM users WHERE id = :user_id")
+    admin_user = db.session.execute(admin_check, {"user_id": user_id}).scalar()
+
+    if admin_user == False:
+        return render_template("usersettings.html", is_admin=is_admin)
+    
+    is_admin = True
+
+    if request.method == "POST":
+        item_name = request.form.get("name")
+        item_price = request.form.get("price")
+        item_category = request.form.get("category")
+
+        sql_item = text("""INSERT INTO items 
+        (name, price, category, time, sold, grade) 
+        VALUES (:item_name, :item_price, :item_category, current_date, 0, 0)""")
+        db.session.execute(sql_item, {"item_name": item_name, "item_price": item_price, "item_category": item_category})
+        db.session.commit()
+
+        notification = "Uusi tuote lisätty valikoimaan."
+
+    sql = text("SELECT id, name, price FROM items")
+
+    result = db.session.execute(sql)
+    items_list = result.fetchall()
+
+    return render_template("item_tools.html", notification=notification, is_admin=is_admin, items=items_list)
+
+
--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE items 
 (id SERIAL PRIMARY KEY, name TEXT, price NUMERIC(7,2), category TEXT, 
-time DATE, sold INT, grade NUMERIC(3,2), grade_count INT);
+time DATE, sold INT, grade NUMERIC(3,2));
 
 CREATE TABLE users 
 (id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN DEFAULT FALSE);
@@ -10,11 +10,11 @@ CREATE TABLE bought
 quantity INT, time DATE);
 
 CREATE TABLE cart 
-(id SERIAL PRIMARY KEY, item_id INT REFERENCES items(id), quantity INT, user_id INT REFERENCES users(id));
+(id SERIAL PRIMARY KEY, item_id INT REFERENCES items(id), user_id INT REFERENCES users(id));
 
 CREATE TABLE reviews 
 (id SERIAL PRIMARY KEY, item_id INT REFERENCES items(id), user_id INT REFERENCES users(id), 
-content TEXT, grade INT);
+content TEXT CHECK (length(content) <= 100), grade INT CHECK (grade >= 1 AND grade <= 5));
 
 CREATE TABLE visitors 
 (id SERIAL PRIMARY KEY, time TIMESTAMP);
